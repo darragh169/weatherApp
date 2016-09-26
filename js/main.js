0c5f1c3228f1a2e15cc4d026bbfb81426eec7b95
darragh169 <darragh169@gmail.com>
2016-09-25 22:27:35 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,162 @@
+function WeatherModel(data){
+    var self = this;
+
+    self.City = ko.observable(new CityModel(data.city));
+
+    var mappedDays = [];
+    for (var i = data.list.length - 1; i >= 0; i--) {
+       mappedDays[i] = new DayModel(data.list[i])
+    }
+
+    self.Days = ko.observableArray(mappedDays);
+    self.currentDay = ko.observable(mappedDays[0]);
+
+    self.setCurrentDay = function(item, e){
+        self.currentDay(item);
+    };
+
+    self.isCurrentDay = function(item){
+        return self.currentDay().datetime === item.datetime;
+    };
+
+    self.map =  ko.observable({
+        lat: ko.observable(self.City().lat()),
+        lng: ko.observable(self.City().lng())
+    });
+
+    self.chart = ko.observable({
+      days: self.Days()
+    });
+
+    self.inputChange = ko.observable();
+}
+
+function CityModel(data) {
+    var self = this;
+
+    self.name = ko.observable(data.name);
+
+    self.country = ko.observable(data.country);
+
+    self.formattedName = ko.computed(function() {
+      return self.name() + ", " + self.country();
+    });
+
+    self.lat = ko.observable(data.coord.lat);
+    self.lng = ko.observable(data.coord.lon);
+}
+
+function DayModel(data){
+    var self = this;
+
+    self.datetime = moment.unix(data.dt).format('dddd Do YYYY');
+    self.temp = data.temp;
+
+    self.weather = ko.observable(data.weather[0]);
+}
+
+function AppViewModel() {
+    var self = this;
+
+    self.Weather = ko.observable();
+    
+    self.getData = function(){
+        $.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily?q=vancouver&APPID=ad1b1e8a90a419b8bff9b0759abf440a&units=imperial", function(data) {
+            if(data.cod === '200'){
+              var WeatherData = new WeatherModel(data)
+              self.Weather(WeatherData);
+              self.appError(false);
+            }else{
+              self.appError(true);
+              $('.dataBlock').hide();
+            }
+        });    
+    }
+
+    self.getCityWeather = function(){
+        $.getJSON("http://api.openweathermap.org/data/2.5/forecast/daily?q="+self.Weather().inputChange()+"&APPID=ad1b1e8a90a419b8bff9b0759abf440a&units=imperial", function(data) {
+            if(data.cod === '200'){
+              var WeatherData = new WeatherModel(data)
+              self.Weather(WeatherData);
+              self.appError(true);
+            }else{
+              self.appError(true);
+              $('.dataBlock').hide();
+            }
+        });    
+    }
+
+    self.appError = ko.observable(false);
+
+    self.getData();
+}
+
+ko.bindingHandlers.map = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var mapObj = ko.utils.unwrapObservable(valueAccessor());
+        var latLng = new google.maps.LatLng(
+            ko.utils.unwrapObservable(mapObj.lat),
+            ko.utils.unwrapObservable(mapObj.lng));
+
+        var mapOptions = {
+          center: latLng,
+          zoom: 5, 
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+
+        mapObj.googleMap = new google.maps.Map(element, mapOptions);
+    }
+};
+
+ko.bindingHandlers.chart = {
+
+    init: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+
+      var chartObj = ko.utils.unwrapObservable(valueAccessor());
+
+      var dates =  chartObj.days.map(function(item){
+        return item.datetime;
+      });
+
+      var Daytemperatures = chartObj.days.map(function(item){
+        return item.temp.day;
+      });
+
+      var chart1 = new Highcharts.Chart({
+          chart: {
+              renderTo: element,
+              type: 'line'
+          },
+          title: {
+              text: 'Temperature for the Week'
+          },
+          xAxis: {
+              categories: dates
+          },
+          yAxis: {
+              title: {
+                  text: "Temperature &de F"
+              }
+          },
+          series: [{
+              name: 'Day Temperature',
+              data: Daytemperatures
+          }
+          ]
+      });
+    },
+    update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+
+    }
+};
+
+ko.bindingHandlers.animateChange = {
+   update: function(element, valueAccessor, allBindings, viewModel, bindingContext) {
+        var value = valueAccessor();
+        var valueUnwrapped = ko.unwrap(value);
+
+        $(element).find('#weatherInfoContainer').hide().fadeIn();
+   }
+}
+
+ko.applyBindings(new AppViewModel(), document.getElementById('WeatherApp'));
